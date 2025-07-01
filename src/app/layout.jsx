6a7ca2e4e8451ada133fb54a16734c3fdd8dd772
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-07-01 16:29:02 +0530

    Animated Background of Navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,6 +14,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "MOJITO",
   description: "An Awwwards Website",
+  icons: {
+    icon: "/images/logo.png",
+  },
 };
 
 export default function RootLayout({ children }) {
